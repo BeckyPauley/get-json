7c14ef4dea0700cb496e423b51b0d1f4068d928f
becky <64131207+BeckyPauley@users.noreply.github.com>
2021-11-11 21:38:57 +0000

    refactor: get astronauts function

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 type people struct {
@@ -15,22 +14,27 @@ type people struct {
 
 func main() {
 
-	url := "http://api.open-notify.org/astros.json"
+	apiUrl := "http://api.open-notify.org/astros.json"
 
-	spaceClient := http.Client{
-		Timeout: time.Second * 2, // timeout fter 2 seconds
+	people, err := getAstros(apiUrl)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("%d people found in space.\n", people.Number)
+}
 
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+func getAstros(apiUrl string) (people, error) {
+	p := people{}
+
+	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	req.Header.Set("UserAgent", "spacecount-tutorial")
 
-	res, err := spaceClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
 	fmt.Printf("HTTP status: %s\n", res.Status)
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,15 +45,14 @@ func main() {
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return p, err
 	}
 
-	p := people{}
 	err = json.Unmarshal(body, &p) // &p passes the value of the empty struct to the method, without the & it will use a different copy of the empty struct
 	if err != nil {
 		log.Fatal(err)
 		log.Fatalf("unable to parse value: %q, error: %s",
 			string(body), err.Error())
 	}
-	fmt.Println(p.Number)
+	return p, nil
 }
